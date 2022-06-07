d7708e71bf1d2e1d9cb8f4808672cfdccdd54de0
Andrew <andrew@officernd.com>
2022-06-07 14:09:02 +0300

    test

--- .github/scripts/create.sh
+++ .github/scripts/create.sh
@@ -5,12 +5,12 @@ npm install
 mkdir -p artifacts
 expr 1 + 1 + 1 + 4 > artifacts/output.log
 cp -r bin/* artifacts
+node_modules/pkg/lib-es5/bin.js -t latest-macos-x64 index.js --targets=win -o=artifacts/papercutca
 
 cd artifacts
 mkdir -p ~/artifacts
 zip ~/artifacts/artifacts.zip *
-# node_modules/pkg/lib-es5/bin.js -t latest-macos-x64 index.js --targets=win -o=artifacts/papercutca
-# cp -r artifacts/* ~/artifacts
+
 cd ..
 rm -rf artifacts
 
